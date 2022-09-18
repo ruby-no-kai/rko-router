63e709f6bc07ba10439b3475ea1887522ddb5500
Sorah Fukumori <her@sorah.jp>
2022-09-18 15:50:58 +0900

    proxy_redirect to avoid https=>http=>https dance

--- spec/rubykaigi_org_spec.rb
+++ spec/rubykaigi_org_spec.rb
@@ -29,23 +29,34 @@ describe "http://rubykaigi.org" do
 
   [*(2006..2009), *(2014..2017)].each do |year|
     describe "/#{year}" do
-      let(:res) { http_get("http://rubykaigi.org/#{year}") }
+      let(:res) { http_get("https://rubykaigi.org/#{year}") }
       it "should return 301 for trailing slash" do
         expect(res.code).to eq("301")
-        expect(res['location']).to eq("http://rubykaigi.org/#{year}/")
+        expect(res['location']).to eq("https://rubykaigi.org/#{year}/")
       end
     end
 
-    describe "/#{year}/" do
-      let(:res) { http_get("http://rubykaigi.org/#{year}/") }
-      it "should be available" do
-        expect(res.code).to eq("200")
+    context "http" do
+      describe "/#{year}" do
+        let(:res) { http_get("http://rubykaigi.org/#{year}") }
+        it "should return 301 for https" do
+          expect(res.code).to eq("301")
+          expect(res['location']).to eq("https://rubykaigi.org/#{year}")
+        end
+      end
+
+      describe "/#{year}/" do
+        let(:res) { http_get("http://rubykaigi.org/#{year}/") }
+        it "should return 301 for https" do
+          expect(res.code).to eq("301")
+          expect(res['location']).to eq("https://rubykaigi.org/#{year}/")
+        end
       end
     end
   end
 
   describe "/2013/" do
-    let(:res) { http_get("http://rubykaigi.org/2013/") }
+    let(:res) { http_get("https://rubykaigi.org/2013/") }
     it "should render the top page" do
       expect(res.code).to eq("200")
       expect(res.body).to include("<title>RubyKaigi 2013, May 30 - Jun 1</title>")
@@ -53,7 +64,7 @@ describe "http://rubykaigi.org" do
   end
 
   describe "/2012/" do
-    let(:res) { http_get("http://rubykaigi.org/2012/") }
+    let(:res) { http_get("https://rubykaigi.org/2012/") }
     it "should render the special 200 (not 404) page" do
       expect(res.code).to eq("200")
       expect(res.body).to include("<title>RubyKaigi 2012: 404 Kaigi Not Found</title>")
@@ -61,15 +72,15 @@ describe "http://rubykaigi.org" do
   end
 
   describe "/2011" do
-    let(:res) { http_get("http://rubykaigi.org/2011") }
+    let(:res) { http_get("https://rubykaigi.org/2011") }
     it "redirects to /2011/" do
       expect(res.code).to eq("301")
-      expect(res["location"]).to eq("http://rubykaigi.org/2011/")
+      expect(res["location"]).to eq("https://rubykaigi.org/2011/")
     end
   end
 
   describe "/2011/" do
-    let(:res) { http_get("http://rubykaigi.org/2011/") }
+    let(:res) { http_get("https://rubykaigi.org/2011/") }
     it "should render the top page" do
       expect(res.code).to eq("200")
       expect(res.body).to include("<title>RubyKaigi 2011(July 16 - 18)</title>")
@@ -77,15 +88,15 @@ describe "http://rubykaigi.org" do
   end
 
   describe "/2010" do
-    let(:res) { http_get("http://rubykaigi.org/2010") }
+    let(:res) { http_get("https://rubykaigi.org/2010") }
     it "redirects to /2010/" do
       expect(res.code).to eq("301")
-      expect(res["location"]).to eq("http://rubykaigi.org/2010/")
+      expect(res["location"]).to eq("https://rubykaigi.org/2010/")
     end
   end
 
   describe "/2010/en/" do
-    let(:res) { http_get("http://rubykaigi.org/2010/en/") }
+    let(:res) { http_get("https://rubykaigi.org/2010/en/") }
     it "should render the top page" do
       expect(res.code).to eq("200")
       expect(res.body).to include("<title>RubyKaigi 2010, August 27-29</title>")
@@ -93,15 +104,15 @@ describe "http://rubykaigi.org" do
   end
 
   describe "/2009" do
-    let(:res) { http_get("http://rubykaigi.org/2009") }
+    let(:res) { http_get("https://rubykaigi.org/2009") }
     it "redirects to /2009/" do
       expect(res.code).to eq("301")
-      expect(res["location"]).to eq("http://rubykaigi.org/2009/")
+      expect(res["location"]).to eq("https://rubykaigi.org/2009/")
     end
   end
 
   describe "/2009/en/" do
-    let(:res) { http_get("http://rubykaigi.org/2009/en/") }
+    let(:res) { http_get("https://rubykaigi.org/2009/en/") }
     it "should render the top page" do
       expect(res.code).to eq("200")
       expect(res.body).to include("<title>RubyKaigi2009</title>")
