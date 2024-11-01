ccdb7b76cfdc4bebf189eaec7e07f68617cac59e
Sorah Fukumori <her@sorah.jp>
2024-11-01 14:55:03 +0900

    rko-router.rubykaigi.net for HTTPS record

---
+++ tf/route53.tf
@@ -0,0 +1,44 @@
+resource "aws_route53_record" "rko-router-rk-n_A" {
+  zone_id  = each.value
+  for_each = local.rubykaigi_net_zones
+  name     = "rko-router.rubykaigi.net."
+  type     = "A"
+  alias {
+    name                   = aws_cloudfront_distribution.rko-router.domain_name
+    zone_id                = aws_cloudfront_distribution.rko-router.hosted_zone_id
+    evaluate_target_health = true
+  }
+}
+resource "aws_route53_record" "rko-router-rk-n_AAAA" {
+  zone_id  = each.value
+  for_each = local.rubykaigi_net_zones
+  name     = "rko-router.rubykaigi.net."
+  type     = "AAAA"
+  alias {
+    name                   = aws_cloudfront_distribution.rko-router.domain_name
+    zone_id                = aws_cloudfront_distribution.rko-router.hosted_zone_id
+    evaluate_target_health = true
+  }
+}
+resource "aws_route53_record" "rko-router-rk-n_HTTPS" {
+  for_each = local.rubykaigi_net_zones
+  name     = "rko-router.rubykaigi.net."
+  zone_id  = each.value
+  type     = "HTTPS"
+  ttl      = 60
+  records  = ["1 . alpn=\"h3,h2\""]
+}
+
+data "aws_route53_zone" "rubykaigi-net_public" {
+  name         = "rubykaigi.net."
+  private_zone = false
+}
+
+data "aws_route53_zone" "rubykaigi-net_private" {
+  name         = "rubykaigi.net."
+  private_zone = true
+}
+
+locals {
+  rubykaigi_net_zones = toset([data.aws_route53_zone.rubykaigi-net_public.zone_id, data.aws_route53_zone.rubykaigi-net_private.zone_id])
+}
