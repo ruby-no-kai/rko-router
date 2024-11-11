fafa371074c6ebf85927f41e6c4ff52ae22710ab
Sorah Fukumori <her@sorah.jp>
2024-11-11 15:56:54 +0900

    Configure proxy_redirect for github pages hosted

--- spec/regional_rubykaigi_org_spec.rb
+++ spec/regional_rubykaigi_org_spec.rb
@@ -12,33 +12,17 @@ describe "http://regional.rubykaigi.org" do
   %w(
     /
     /oedo03/
-    /osaka01/
-    /kansai2017/
-    /kansai06/
     /kansai05/
-    /chuork01/
-    /shibuya01/
-    /tokyu12/
-    /tokyu11/
-    /tokyu09/
-    /tokyu08/
-    /kana01/
     /oedo04/
     /hamamatsu01/
     /oedo05/
     /oedo06/
-    /oedo10/
-    /tokyo12/
-    /tokyo11/
-    /tokyu10/
-    /kwsk01/
   ).each do |path|
     describe(path) do
       let(:res) { http_get("https://regional.rubykaigi.org#{path}") }
       it "returns ok" do
         pending 'kanrk05.herokuapp.com is down' if path == '/kansai05/'
         pending 'http://rubykaigi-hamamatsu.s3-website-ap-northeast-1.amazonaws.com/hamamatsu01/ returns C-T:application/javascript' if path == '/hamamatsu01/'
-        pending 'asakusa.github.io returns 301 (#110)' if path == '/oedo10/'
 
         expect(res.code).to eq("200")
         expect(res["content-type"]).to include("text/html")
@@ -46,18 +30,55 @@ describe "http://regional.rubykaigi.org" do
     end
   end
 
-  %w(
-    tokyo12
-  ).each do |subdir|
-    describe("/#{subdir}") do
-      let(:res) { http_get("https://regional.rubykaigi.org/#{subdir}") }
-      it "may redirect to a path with trailing slash" do
-        case res.code
-        when "200"
-          expect(res["content-type"]).to include("text/html")
-        when /^3/
-          expect(res["location"]).to include("regional.rubykaigi.org/#{subdir}")
-          expect(res["location"]).not_to include("github.io/")
+
+  describe "GitHub Pages hosted subdirectories" do
+    %w(
+      tokyo12
+      osaka04
+      osaka03
+      osaka02
+      osaka01
+      kansai08
+      kansai2017
+      kansai06
+      chuork01
+      okrk01
+      shibuya01
+      tokyu13
+      tokyu12
+      tokyu11
+      tokyu09
+      tokyu08
+      kana01
+      oedo10
+      tokyo12
+      tokyo11
+      tokyu10
+      kwsk01
+    ).each do |subdir|
+      describe(subdir) do
+        describe "/#{subdir}" do
+          let(:res) { http_get("https://regional.rubykaigi.org/#{subdir}") }
+          it "may redirect to a path with trailing slash" do
+            case res.code
+            when "200"
+              expect(res["content-type"]).to include("text/html")
+            when /^3/
+              expect(res["location"]).to include("regional.rubykaigi.org/#{subdir}/")
+              expect(res["location"]).not_to include("github.io/")
+            end
+          end
+        end
+
+        describe "/#{subdir}/" do
+          let(:res) { http_get("https://regional.rubykaigi.org/#{subdir}/") }
+          it "returns ok" do
+            #pending 'kanrk05.herokuapp.com is down' if path == '/kansai05/'
+            #pending 'http://rubykaigi-hamamatsu.s3-website-ap-northeast-1.amazonaws.com/hamamatsu01/ returns C-T:application/javascript' if path == '/hamamatsu01/'
+            #pending 'asakusa.github.io returns 301 (#110)' if path == '/oedo10/'
+            expect(res.code).to eq("200")
+            expect(res["content-type"]).to include("text/html")
+          end
         end
       end
     end
@@ -68,7 +89,6 @@ describe "http://regional.rubykaigi.org" do
     /matsue08/ matsue.rubyist.net
     /matrk07/ matsue.rubyist.net
     /matsue07/ matsue.rubyist.net
-    /okrk01/ ruby.okinawa
   ).each_slice(2) do |path, host|
     describe(path) do
       let(:res) { http_get("https://regional.rubykaigi.org#{path}") }
