015dc8c444cf5fe489f4790f4a09f794536f59ab
Sorah Fukumori <her@sorah.jp>
2025-11-11 15:53:39 +0900

    tf: default_tags

--- tf/aws.tf
+++ tf/aws.tf
@@ -1,18 +1,36 @@
 provider "aws" {
   region              = "us-west-2"
   allowed_account_ids = ["005216166247"]
+
+  default_tags {
+    tags = {
+      Project = "rko-router"
+    }
+  }
 }
 
 provider "aws" {
   alias               = "apne1"
   region              = "ap-northeast-1"
   allowed_account_ids = ["005216166247"]
+
+  default_tags {
+    tags = {
+      Project = "rko-router"
+    }
+  }
 }
 
 provider "aws" {
   alias               = "use1"
   region              = "us-east-1"
   allowed_account_ids = ["005216166247"]
+
+  default_tags {
+    tags = {
+      Project = "rko-router"
+    }
+  }
 }
 
 data "aws_caller_identity" "current" {}
