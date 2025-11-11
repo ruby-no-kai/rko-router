073281792e6be5f22e7d90f8aacf0955776c76dd
Sorah Fukumori <her@sorah.jp>
2025-11-11 16:34:50 +0900

    ci: deploy to lambda

--- tf/lambda.tf
+++ tf/lambda.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "rko-router" {
   function_name = "rko-router"
 
   package_type  = "Image"
-  image_uri     = "${aws_ecr_repository.rko-router.repository_url}:3285c10bdd196708e97523c116baeba981b8b8a7"
+  image_uri     = "${aws_ecr_repository.rko-router.repository_url}:7e1c5410f5b39e9be0d8396509b44dfcdb77d43d"
   architectures = ["x86_64"]
 
   role = aws_iam_role.LambdaRkoRouter.arn
@@ -23,11 +23,11 @@ resource "aws_lambda_function" "rko-router" {
     Name = "rko-router"
   }
 
-  # lifecycle {
-  #   ignore_changes = [
-  #     image_uri,
-  #   ]
-  # }
+  lifecycle {
+    ignore_changes = [
+      image_uri,
+    ]
+  }
 }
 
 resource "aws_lambda_function_url" "rko-router" {
